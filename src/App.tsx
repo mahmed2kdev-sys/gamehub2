ee0962589bd1462de7b7e59b52d4a43ea344a143
Mahfuz Ahmed <m4mahfuz@gmail.com>
2026-09-03 12:47:14 +0600

    feat: infinite scroll via react-infinite-scroll-component (window mode)

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,10 @@ function App() {
 
   return (
     <Grid
-      h="100dvh"
       templateAreas={{
         base: '"nav" "main"',
         lg: '"nav nav" "aside main"',
       }}
-      gridTemplateRows={{ base: 'auto 1fr auto', lg: 'auto 1fr' }}
       gridTemplateColumns={{ base: '1fr', lg: '300px 1fr' }}
     >
       <GridItem gridArea="nav" bg={{ _light: 'gray.50', _dark: 'gray.800' }} p="4">
@@ -27,7 +25,7 @@ function App() {
       <GridItem gridArea="aside" bg={{ _light: 'gray.50', _dark: 'gray.800' }} color="fg" p="4" hideBelow="lg">
         <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })} />
       </GridItem>
-      <GridItem gridArea="main" bg={{ _light: 'gray.50', _dark: 'gray.800' }} color="fg" p="4" overflowY="auto">
+      <GridItem gridArea="main" bg={{ _light: 'gray.50', _dark: 'gray.800' }} color="fg" p="4">
         <GameHeading gameQuery={gameQuery} />
         <HStack mb={4}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
