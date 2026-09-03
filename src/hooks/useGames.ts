24dd49c30062615f03b193f7aaa1e656e5b82852
Mahfuz Ahmed <m4mahfuz@gmail.com>
2026-09-03 13:20:46 +0600

    feat: add 24h staleTime to useGames
    Co-Authored-By: internal-model

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,6 +7,7 @@ export default function useGames(gameQuery: GameQuery) {
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam, signal }) => gameService.getGames(gameQuery, pageParam as number, signal),
     initialPageParam: 1,
+    staleTime: 24 * 60 * 60 * 1000,
     getNextPageParam: (lastPage, allPages) => (lastPage.next ? allPages.length + 1 : undefined),
   });
 }
